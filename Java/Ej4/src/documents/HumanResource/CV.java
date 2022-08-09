843fafcda3ab034557f751b8619e6d5bb1e7d846
Pablo Guendulain <pguendulain92@gmail.com>
2022-08-09 12:14:05 -0300

    GoingUpFolderJava

---
+++ Java/Ej4/src/documents/HumanResource/CV.java
@@ -0,0 +1,10 @@
+package documents.HumanResource;
+import documents.Document;
+
+public class CV extends Document {
+    @Override
+    public String toString() {
+        return "CV for you";
+    }
+
+}
